9a8996de7944deec342473895aa3355d0880c4ce
Nghia Nguyen <nnnghia98@gmail.com>
2026-04-16 18:30:23 +0700

    feat(bot): enable local maintenance mode

--- bot/commands/maintainance/index.js
+++ bot/commands/maintainance/index.js
@@ -1,7 +1,7 @@
 const maintenanceMessage = () => {
   return `🚫 *THÔNG BÁO NGỪNG HOẠT ĐỘNG*
 
-Bot sẽ chính thức *ngừng hoạt động vĩnh viễn* kể từ *16/04/2026*.
+Vì một số hạn chế trong cơ cấu luật mới đã áp đặt lên nhà phát triển nên bot sẽ chính thức *ngừng hoạt động vĩnh viễn* kể từ *16/04/2026*.
 
 Cảm ơn mọi người đã đồng hành và sử dụng bot trong suốt thời gian qua.
 
